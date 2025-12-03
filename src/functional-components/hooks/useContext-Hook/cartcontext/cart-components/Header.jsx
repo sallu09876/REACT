519bfed53c72d678eff7719780b34a3af76b0b15
Salman Rasheed <sallu2004mkt@gmail.com>
2025-12-03 09:40:04 +0530

    r

--- src/functional-components/hooks/useContext-Hook/cartcontext/cart-components/Header.jsx
+++ src/functional-components/hooks/useContext-Hook/cartcontext/cart-components/Header.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CartContext } from '../CartContext'
 
 const Header = () => {
+
+  const { cart } = useContext(CartContext);
+
+  const totalqty = cart.reduce((sum, item) => sum + item.qty, 0);
+
+  const totalprice = cart.reduce((sum, item) => sum + item.price, 0);
+
   return (
-    <div>
-      
-    </div>
+    <header className='header'>
+      <h1>My Shopping Cart</h1>  
+      <div className='summary'>
+        <p>Items: { totalqty }</p>
+        <p>Price: { totalprice }</p>
+      </div>
+    </header>
   )
 }
 
